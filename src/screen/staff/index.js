4d21f75846ed977e54c90f5c78b27501d61358ba
mani3710 <mani37105@gmail.com>
2022-05-28 22:56:14 +0530

    update : staff selecting is done

--- src/screen/staff/index.js
+++ src/screen/staff/index.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import { useNavigate } from 'react-router-dom';
+import StaffList from './staffList';
 const Staff = () => {
     const navigation = useNavigate();
+    const [flowNo, setFlowNo] = useState(1)
+    const renderContent = () => {
+        switch (flowNo) {
+            case 1:
+                return <StaffList />
+                break;
+            default:
+                return null;
+        }
+    }
     return (
         <div>
-            {/* <header class="header" id="header">
-                <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
-                <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt="" /> </div>
-            </header> */}
-            <div class="l-navbar" id="nav-bar" style={{ width: 200 }}>
+
+            <div class="l-navbar" id="nav-bar" style={{ width: 230 }}>
                 <nav class="nav">
                     <div> <a class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name" style={{ fontSize: 15, marginLeft: -10 }}>ANNA UNIVERSITY</span> </a>
                         <div class="nav_list">
                             <label onClick={() => { navigation("/home") }} class="nav_link"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </label>
                             <label onClick={() => { navigation("/finalmark") }} class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Mark</span> </label>
-                            <label onClick={() => { navigation("/downloadpdf") }} class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Download PDF</span> </label>
+                            {/* <label onClick={() => { navigation("/downloadpdf") }} class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Download PDF</span> </label> */}
                             <label class="nav_link active"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Staff</span> </label>
                             <label onClick={() => { navigation("/student") }} class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Student</span> </label>
 
@@ -24,8 +32,8 @@ const Staff = () => {
                 </nav>
             </div>
 
-            <div class="height-100 bg-light" style={{ marginTop: -65, width: "89.5vw", marginLeft: 100 }}>
-                <h4>Staff</h4>
+            <div class="height-100 bg-light" style={{ marginTop: -65, width: "91.3%", marginLeft: 130 }}>
+                <center>{renderContent()}</center>
             </div>
 
         </div>
---
+++ src/screen/student/studentList/index.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+
+import { getProjectList, createNewProject, createBatch, setSelectedProjectForViewMore } from '../../../redux/reducer/projectlist';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Drawer from 'react-drag-drawer';
+import { v4 as uuidv4 } from 'uuid';
+import { Button, ButtonGroup, Container, ButtonToolbar, Jumbotron, Card } from 'react-bootstrap';
+import { getStudentList } from '../../../redux/reducer/student';
+const StudentList = (props) => {
+    const dispatch = useDispatch();
+
+    //Store 
+    const authStore = useSelector(state => state.auth);
+    const projectlistStore = useSelector(state => state.projectlist);
+    const studentStore = useSelector(state => state.student);
+    const {
+        authLoader,
+        adminDetails
+    } = authStore;
+    const {
+        projectLoader,
+        projectListData,
+        currentProjectDetails
+    } = projectlistStore;
+    const {
+        studentListData
+    } = studentStore;
+    const [isShowCreateProject, setIsShowCreateProject] = useState(false);
+    const [newProjectName, setNewProjectName] = useState("");
+    const [projectCreateError, setProjectCreateError] = useState("");
+    const [noOfBatch, setNoOfBatch] = useState(1);
+    const [isShowBatch, setIsShowBatch] = useState(false);
+    const [batchCreateError, setBatchCreateError] = useState("");
+
+    useEffect(() => {
+        getStaffListFunc()
+    }, [])
+    const getStaffListFunc = () => {
+        dispatch(getStudentList());
+    }
+
+
+
+
+    return (
+        <center>
+            <div class="height-100 bg-light" style={{ width: "95%", backgroundColor: "red" }}>
+                <center><label style={{ marginTop: 20, fontWeight: "bold", fontSize: 30, marginBottom: 20 }}>STAFF</label></center>
+                <div class="d-flex flex-row-reverse bd-highlight">
+
+
+                    <div class="p-2 bd-highlight">
+                        <button
+                            onClick={() => {
+                                // setIsShowCreateProject(true)
+                            }}
+                            type="button" class="btn btn-warning">CREATE STAFF</button>
+                    </div>
+                    {/* <div class="p-2 bd-highlight">
+                        <label style={{ fontWeight: "bold", marginTop: 5 }}>{adminData.username}</label>
+                    </div>
+                    <div class="p-2 bd-highlight">
+                        <label style={{ marginTop: 5 }}>User :</label>
+                    </div> */}
+                </div>
+                <div style={{ overflow: "auto", height: "82vh" }}>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Sno</th>
+                                <th scope="col">Roll no</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {studentListData.map((item, i) => {
+                                return (
+                                    <tr>
+                                        <th scope="col">{i + 1}</th>
+                                        <th scope="col" >{item.rollno}</th>
+                                        <th scope="col">{item.username}</th>
+                                        <th scope="col" >{item.email}</th>
+
+                                    </tr>
+                                )
+                            })}
+
+
+
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+        </center >
+    )
+}
+
+export default StudentList;
